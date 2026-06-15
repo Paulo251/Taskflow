a3cd145d8db70b96bead51f15c13599170d23b31
paulo251 <paulorico1100@gmail.com>
2026-06-15 16:08:17 -0300

    feat:bycript para usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
+    has_many :tasks
+    has_secure_password
+
     validates :name, presence: true, 
                     length: { minimum: 3 }
 
     validates :email, presence: true,
                     uniqueness: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP }
-
-    has_many :tasks
 end
 
